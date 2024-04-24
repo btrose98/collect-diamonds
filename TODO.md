1b97017c6c2de73a6d48884d334a4b3d5dd033f5
Bradley Rose <rose.bradley9@gmail.com>
2024-04-24 13:59:09 -0400

    Updated scrypt to work with current website implementation of coinmarketcap.com and added a todo

---
+++ TODO.md
@@ -0,0 +1,4 @@
+- [] Implement captcha bypass if required
+(https://github.com/ultrafunkamsterdam/undetected-chromedriver/issues/735)
+- 
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import time
+import yaml
 from selenium import webdriver
-import yaml, time
+from selenium.webdriver.common.by import By
 
-conf = yaml.load(open('loginDetails.yml'))
+conf = yaml.safe_load(open('loginDetails.yml'))
 
 username = conf['coinmarketcap_user']['email']
 password = conf['coinmarketcap_user']['password']
@@ -10,18 +12,29 @@ driver = webdriver.Chrome()
 
 driver.get('https://coinmarketcap.com/account/my-diamonds/')
 try:
-    driver.find_element_by_css_selector('#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div:nth-child(2) > div.sc-1snuar3-4.dCKehS > button').click()
-    time.sleep(1)
-    
-    driver.find_element_by_css_selector('body > div.t8xka3-0.clxZon.modalOpened > div > div > div > div:nth-child(3) > input').send_keys(username)
-    driver.find_element_by_css_selector('body > div.t8xka3-0.clxZon.modalOpened > div > div > div > div.sc-1htht4q-3.kHSKLo.last > div.sc-1srpo52-0.ciHfxX > input').send_keys(password)
-    driver.find_element_by_css_selector('body > div.t8xka3-0.clxZon.modalOpened > div > div > div > div.sc-1htht4q-6.kUXNCx > button').click()
+    login_button_selector = "#__next > div.sc-e742802a-1.fprqKh.global-layout-v2 > div > div.cmc-body-wrapper > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div:nth-child(2) > div.sc-8a2da6a1-4.kugsrA > div.sc-8a2da6a1-11.jgVnqT > button"
+    driver.find_element(By.CSS_SELECTOR, login_button_selector).click()
+
+    login_tab_selector = "body > div.sc-acb6320-0.jVSkTR.modalOpened > div > div > div > div > div.sc-79c2884f-1.hmAaQh > div.sc-79c2884f-2.jEAjov.tab-item"
+    driver.find_element(By.CSS_SELECTOR, login_tab_selector).click()
+
+    email_input_selector = "body > div.sc-acb6320-0.jVSkTR.modalOpened > div > div > div > div > div:nth-child(2) > div:nth-child(1) > input"
+    driver.find_element(By.CSS_SELECTOR,email_input_selector).send_keys(username)
+
+    password_input_selector = "body > div.sc-acb6320-0.jVSkTR.modalOpened > div > div > div > div > div:nth-child(2) > div.sc-1cf81b5e-8.kQPJck.last > div.sc-f23c2c4c-0.gEqHyg > input"
+    driver.find_element(By.CSS_SELECTOR, password_input_selector).send_keys(password)
+
+    login_with_credentials_button_selector = "body > div.sc-acb6320-0.jVSkTR.modalOpened > div > div > div > div > div:nth-child(2) > div.sc-1cf81b5e-11.hcnPEY > button"
+    driver.find_element(By.CSS_SELECTOR,login_with_credentials_button_selector).click()
     time.sleep(1)
 
-    driver.find_element_by_css_selector('#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div:nth-child(2) > div.sc-1snuar3-4.dCKehS > button').click()
+    collect_diamonds_button_selector = "#__next > div.sc-e742802a-1.fprqKh.global-layout-v2 > div > div.cmc-body-wrapper > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div:nth-child(2) > div.sc-8a2da6a1-4.kugsrA > div.sc-8a2da6a1-11.jgVnqT > button"
+    driver.find_element(By.CSS_SELECTOR, collect_diamonds_button_selector).click()
 
-except:
-    print('---- An error has occured while accessing the website. ----')
+except Exception as e:
+    print('---- An error has occured while accessing the website. ----\n')
+    print(f"Error:\n{e}")
 
 finally:
+    driver.quit()
     exit()
