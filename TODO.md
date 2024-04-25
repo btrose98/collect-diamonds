1d6d7c6f9f5a28815d04cf586343bd5fc035e10a
Bradley Rose <rose.bradley9@gmail.com>
2024-04-25 12:41:52 -0400

    Switched to using undetected chromedriver and python 3.11

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
-- [] Implement captcha bypass if required
-(https://github.com/ultrafunkamsterdam/undetected-chromedriver/issues/735)
+- [] Test and verify script works as expected.
+- [] Create a docker container to be executed every 24h? (hosted on raspberry pi)
+- [] Implement captcha bypass
+(https://github.com/ultrafunkamsterdam/undetected-chromedriver/issues/735).
 - 
 
--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import time
 import yaml
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+
 
 conf = yaml.safe_load(open('loginDetails.yml'))
 
 username = conf['coinmarketcap_user']['email']
 password = conf['coinmarketcap_user']['password']
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
+driver = uc.Chrome()
 
 driver.get('https://coinmarketcap.com/account/my-diamonds/')
 try:
     login_button_selector = "#__next > div.sc-e742802a-1.fprqKh.global-layout-v2 > div > div.cmc-body-wrapper > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div:nth-child(2) > div.sc-8a2da6a1-4.kugsrA > div.sc-8a2da6a1-11.jgVnqT > button"
     driver.find_element(By.CSS_SELECTOR, login_button_selector).click()
+    time.sleep(1)
 
     login_tab_selector = "body > div.sc-acb6320-0.jVSkTR.modalOpened > div > div > div > div > div.sc-79c2884f-1.hmAaQh > div.sc-79c2884f-2.jEAjov.tab-item"
     driver.find_element(By.CSS_SELECTOR, login_tab_selector).click()
@@ -23,6 +27,7 @@ try:
 
     password_input_selector = "body > div.sc-acb6320-0.jVSkTR.modalOpened > div > div > div > div > div:nth-child(2) > div.sc-1cf81b5e-8.kQPJck.last > div.sc-f23c2c4c-0.gEqHyg > input"
     driver.find_element(By.CSS_SELECTOR, password_input_selector).send_keys(password)
+    time.sleep(5)
 
     login_with_credentials_button_selector = "body > div.sc-acb6320-0.jVSkTR.modalOpened > div > div > div > div > div:nth-child(2) > div.sc-1cf81b5e-11.hcnPEY > button"
     driver.find_element(By.CSS_SELECTOR,login_with_credentials_button_selector).click()
